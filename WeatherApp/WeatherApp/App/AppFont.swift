cb3097b06d14eefe0878325f9f4779d98f9153b2
Dzmitry Liashchou <liashchou.d@gmail.com>
2024-07-19 23:53:04 +0200

    Add necessary fonts and color

---
+++ WeatherApp/WeatherApp/App/AppFont.swift
@@ -0,0 +1,13 @@
+//
+//  AppFont.swift
+//  WeatherApp
+//
+//  Created by Dzmitry Liashchou on 19/07/2024.
+//
+
+struct AppFont {
+    static let interBold: String = "Inter-Bold"
+    static let interMedium: String = "Inter-Medium"
+    static let interRegular: String = "Inter-Regular"
+    static let interExtraLight: String = "Inter-ExtraLight"
+}
